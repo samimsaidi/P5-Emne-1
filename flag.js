2476c42b74687de4c10a45429b307c519ab396d9
Samim <69625893+samimsaidi@users.noreply.github.com>
2020-08-18 12:49:55 +0200

    Update flag.js

--- flag.js
+++ flag.js
@@ -1,40 +1,36 @@
-//var clearBackground = false;
+
 function setup() {
-  createCanvas(600, 600);
-  background(0);
+  createCanvas(600, 350) // Laver canvas som skaber             baggrunden med en størrelse på 500 og 200
+  console.log("Funktion setup [JA]"); // Kommentar for at       tjekke for debugs (hvis der er nogen)
+  
 }
 
 function draw() {
-  //if (clearBackground) {
-  //background(0);
-  //clearBackground = false; 
-  if (mouseIsPressed) { //mouseIfPressed is a "boolean" variable
-    //true or false
-    noStroke();
-    fill(255);
-    //rotate(mouseX*0.5)
-    star(mouseX, mouseY, 5, 11.6, 5);
-    //translate(width * 0.8, height * 0.5);
-    //rotate(frameCount / -100.0);
+  let flag = 0;
+  if (mouseIsPressed) {
+    if(flag === 0) {
+    flag++
+    japan();
+    }
+    
+    if(flag === 1) {
+    flag++
+    frankrig();
+    }
   }
 }
-//star function from p5.js reference
-function star(x, y, radius1, radius2, npoints) {
-  var angle = TWO_PI / npoints;
-  var halfAngle = angle / 2.0;
-  beginShape();
-  for (var a = 0; a < TWO_PI; a += angle) {
-    var sx = x + cos(a) * radius2;
-    var sy = y + sin(a) * radius2;
-    vertex(sx, sy);
-    sx = x + cos(a + halfAngle) * radius1;
-    sy = y + sin(a + halfAngle) * radius1;
-    vertex(sx, sy);
-  }
-  endShape(CLOSE);
+
+
+function japan() {
+  background(255)
+  noStroke();
+  fill(255,0,0);
+  circle(300,175,100);
 }
-//function keyTyped() {
-//if (key === 'a') {
-//clearBackground = true
-//}
-//}
+
+function frankrig() {
+  background(255)
+  noStroke();
+  fill(255,0,0);
+  rect(244, 200, 13, 120); // rect(x, y, width, height)
+}
